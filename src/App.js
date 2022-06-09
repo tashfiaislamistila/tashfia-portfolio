10d9faecf2bad5664e3acd0bddf22657ba87b5b5
tashfiaislamistila <tashfia.cse@gmail.com>
2022-06-09 00:06:50 -0700

    set navbar commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 import Navbar from './Page/Shared/Navbar';
+import Home from './Page/Home/Home';
+import About from './Page/Home/About';
 
 function App() {
   return (
     <div >
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
