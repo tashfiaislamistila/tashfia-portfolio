8f26dde18ed7f96456c4fbd60f1981e7b28a3b6e
tashfiaislamistila <tashfia.cse@gmail.com>
2022-06-09 20:22:41 -0700

    font modification

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from './Page/Shared/Navbar';
 import Home from './Page/Home/Home';
 
-
 function App() {
   return (
     <div>
+
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
--- src/Page/Home/About.js
+++ src/Page/Home/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import picture from '../../assets/mypicture.jpg'
+import Typical from 'react-typical'
+
 const About = () => {
     return (
         <div id="about" class="hero min-h-screen bg-base-200">
@@ -8,7 +10,27 @@ const About = () => {
                 <div>
                     <h1 class="text-2xl font-bold">Hello!</h1>
                     <h1 class="text-5xl font-bold">I am Tashfia Islam</h1>
-                    <p class="py-8 text-2xl">I am a MERN stack web developer trying to gain confidence using my potential in the field of “Web Development”. I will express my innovative creative skills for self and company growth.</p>
+                    <h1 class="text-4xl mt-4 text-error ">
+                        <Typical
+                            loop={Infinity}
+                            wrapper="b"
+                            steps={[
+                                'Web Developer',
+                                900,
+                                'MERN Stack Developer',
+                                900,
+                                'React Developer',
+                                900,
+                                'Javascript Developer',
+                                900,
+                                'Frontend Developer',
+                                900,
+                                'Programmer',
+                                900
+                            ]}
+                        />
+                    </h1>
+                    <p class="py-8 text-2xl">I am a web developer trying to gain confidence using my potential in the field of “Web Development”. I will express my innovative creative skills for self and company growth.</p>
                     <div>
                         <button class="btn btn-outline btn-primary mx-2"><a href='https://drive.google.com/file/d/16tn4DlMUTwzH1wSMEY74uDiMNFEP4hr3/view?usp=sharing' alt='resume link'>Resume</a></button>
                         <button class="btn btn-outline btn-secondary"><a href='https://www.linkedin.com/in/tashfia-islam/' alt='resume link'>Linkdin</a></button>
